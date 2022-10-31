b949af319ae3fd3f672a16e56df17a01f86b9b0c
Lautaro Rocha <lautarovrocha@gmail.com>
2022-10-31 15:38:34 -0300

    generateCards function

--- clase6/pairProgramming/script.js
+++ clase6/pairProgramming/script.js
@@ -12,11 +12,35 @@ let post = 'https://jsonplaceholder.typicode.com/posts/1'
 
 let comments = `https://jsonplaceholder.typicode.com/posts/${postId}/comments`
 
-fetch(rutaPost)
-    .then( res => res.json())
-    .then(data => posts = data)
-
+let section = document.querySelector('section')
 
 fetch(comments)
 .then( res => res.json())
 .then(data => postComments = data)
+
+async function getPosts(){
+   await fetch(rutaPost)
+        .then( res => res.json())
+        .then(data => posts = data)
+        posts.forEach((post) =>{
+            generateCard(post)
+        })
+}
+
+function generateCard(post){
+    let card = document.createElement('div')
+    let title = document.createElement('span')
+    let body = document.createElement('p')
+    let btn = document.createElement('button')
+    
+    title.textContent = post.title;
+    body.textContent = post.body;
+    btn.textContent = 'VER COMENTARIOS'
+    card.setAttribute('class', 'card');
+
+    card.appendChild(title);
+    card.appendChild(body);
+    card.appendChild(btn);
+
+    section.appendChild(card);
+}
